dcc59e3ee76bfa8212885462040447d6ab091437
MARIANA RUBERT GRECCO <55103645+maryrubert@users.noreply.github.com>
2023-07-31 15:05:39 -0300

    Add files via upload

---
+++ calculadora2.py
@@ -0,0 +1,23 @@
+def calculadora (n1, n2, op):
+  calculo = 0
+  if(op==1):
+    calculo = n1+n2
+  elif(op==2):
+    calculo = n1-n2
+  elif(op==3):
+    calculo = n1*n2
+  elif(op==4):
+    calculo = n1/n2
+  return calculo
+
+
+while(True):
+    op = int(input("Escolha a operação: "))
+    if(op==0):
+       break
+
+    n1 = int(input("Digite o primeiro número  "))
+    n2 = int(input("Digite o segundo número  "))
+
+    resultado = calculadora(n1, n2, op)
+    print("Resultado:   ", resultado)
